8ab47023f04086acb9246187c6379d327ea44cfb
Fhh <546588335@qq.com>
2020-12-23 00:13:00 +0800

    test

---
+++ gittest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/23 0:06
+# @Author  : Fhh
+# @File    : gittest.py
+# Good good study,day day up!
+""""""
+if __name__ == '__main__':
+    print("ok")
